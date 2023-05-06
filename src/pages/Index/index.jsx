24e3db4de204b37122da67e155bc78693256d548
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2023-05-06 18:11:08 +0900

    complete rent teams

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 // 导入关于轮播图的组件
-import { Carousel,Flex } from 'antd-mobile-v2';
+import { Carousel,Flex,Grid  } from 'antd-mobile-v2';
 // 导入axios
 import axios from 'axios'
 // 导入导航菜单图片
@@ -8,7 +8,7 @@ import Nav1 from '../../assets/images/nav-1.png'
 import Nav2 from '../../assets/images/nav-2.png'
 import Nav3 from '../../assets/images/nav-3.png'
 import Nav4 from '../../assets/images/nav-4.png'
-import './index.css'
+import './index.scss'
 
 // 导航菜单数据
 const navs=[
@@ -43,7 +43,9 @@ export default class Index extends Component {
     // 初始化轮播图状态数据
     swipers:[],
     // 添加一个状态，表示轮播图数据是否加载完成，等数据加载完成时，把状态值改为true(```isSwiperLoaded:true```)
-    isSwiperLoaded:false
+    isSwiperLoaded:false,
+    // 租房小组数据
+    groups:[]
   }
   // 获取轮播图数据的方法
   async getSwipers(){
@@ -54,9 +56,26 @@ export default class Index extends Component {
         isSwiperLoaded:true
     })
   }
+
+  // 获取租房小组数据的方法
+  async getGroups(){
+    const res=await axios.get('http://localhost:8080/home/groups',{
+      params:{
+        // 暂时用服务器提供的，后续城市位置变化时再把对应城市的地址当做URL参数传递进去即可
+        area:'AREA%7C88cff55c-aaa4-e2e0'
+      }
+    })
+    console.log(res);
+    // 在setState方法中修改并更新所在城市的数据
+    this.setState({
+      groups:res.data.body
+    })
+  }
+
   componentDidMount() {
     // 调用获取轮播图数据的函数，使之能正常渲染轮播图
     this.getSwipers()
+    this.getGroups()
   }
   //渲染轮播图结构   
   renderSwipers(){
@@ -102,6 +121,24 @@ export default class Index extends Component {
         
         {/* 导航菜单 */}
         <Flex className="nav">{this.renderNavs()}</Flex>
+
+        <div className='group'>
+          <h3 className='group-title'>
+            租房小组 <span className='more'>更多</span>
+          </h3>
+          {/* 宫格组件 */}
+          <Grid data={this.state.groups} columnNum={2} square={false} hasLine={false}
+            renderItem={(item)=>{
+              <Flex className='group-item' justify='around' key={item.id}>
+                <div className='desc'>
+                  <p className='title'>{item.title}</p>
+                  <span className='info'>{item.desc}</span>
+                </div>
+                <img src={`http://localhost:8080${item.imgSrc}`} alt="" />
+              </Flex>
+            }}/>
+        </div>
+        
       </div>
     );
   }
